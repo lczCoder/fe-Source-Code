33d8a572fc0448142c3c8eb0a42353b00be05ca9
lczcoder <lczcoder@163.com>
2021-10-06 11:26:16 +0800

    基础响应式写法

--- part-3-vue/src/simple-vue/z-vue.js
+++ part-3-vue/src/simple-vue/z-vue.js
@@ -1,20 +1,47 @@
-class Zvue{
-  constructor(options){
-    console.log('option',options);
+let $el = null;
+class Zvue {
+  constructor(options) {
     this.$options = options;
-    this.$data = options.data
+    this.$data = options.data;
+    $el = options.el
+    observe(this.$data);
+    update(this.$options.el,this.$data.count)
   }
 }
 
-class Observe{
-
+class Observe {
+  constructor(obj) {
+    this.obj = obj;
+    this.walk(obj);
+  }
+  walk(obj) {
+    Object.keys(obj).forEach((key) => {
+      defineReactive(obj,key, obj[key]);
+    });
+  }
 }
 
-function observe(){
-
+function observe(data) {
+  if (typeof data !== "object" || data == null) {
+    return;
+  }
+  new Observe(data);
 }
 
-function defineReactive(){
-
+function defineReactive(obj, key, value) {
+  Object.defineProperty(obj, key, {
+    get() {
+      return value;
+    },
+    set(newVal) {
+      if (newVal !== value) {
+        value = newVal;
+        update($el,newVal);
+      }
+    },
+  });
 }
 
+function update(el,value){
+ document.querySelector(el).innerText = value
+}
