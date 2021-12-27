620cdcc16cfc7a5da18bb083c530504f01967e3f
linchengzhe <linchengzhe@wandu.cn>
2021-12-27 17:44:16 +0800

    pref:类型别名补充说明

--- typescript/src/5、aliastype.ts
+++ typescript/src/5、aliastype.ts
@@ -17,4 +17,20 @@ function getout<T>(x:T):T{
 
 getout<GetSomeThing>(123)
 
+
+// 字符串字面量限制
+type SelectString = 'name' | 'age' | number | boolean 
+let x:SelectString = 'name'
+x = 'age'
+x = 123
+x = true
+// x = '123' //error 变量类型只能是联合类型中限制的，如果是字符串就限制只允许赋值定义的固定值
+
+
+/**
+ * 1、一般会把比较复杂的联合类型，定义一个别名，简化书写流程
+ * 2、联合变量是类型的限制, 别名是类型和值的限制。
+ */
+
+
 export {}
