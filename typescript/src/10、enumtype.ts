8d20b5838a2ffcad8c014c9005da0c51dfab061f
lczCoder <lczcoder@163.com>
2022-01-05 23:10:12 +0800

    pref:枚举补充

--- typescript/src/10、enumtype.ts
+++ typescript/src/10、enumtype.ts
@@ -1,12 +1,20 @@
 // 枚举
+// 1、双向映射关系（仅限于数字下标）
+// 2、支持遍历 （下标和属性）
+
 enum StrEnum {
   one = 'one',
   two = 'two'
 }
 
 enum Dates {
-  one,
-  two,
+  cat,
+  dog,
+}
+
+for (let i in Dates){
+  console.log(i);
+  
 }
 
 
